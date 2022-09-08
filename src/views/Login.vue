ce5713bf3788a344fc1db5d92d8531313795ab5f
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-09-08 01:55:43 -0300

    Aula 04 - Parte 01 - Integração API

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,12 +3,16 @@ import { defineComponent } from 'vue';
 import InputPublico from '../components/InputPublico.vue';
 import iconeLogin from '../assets/imagens/login.svg';
 import iconeSenha from '../assets/imagens/senha.svg';
+import { LoginServices } from '../services/LoginServices';
+
+const loginService = new LoginServices();
 
 export default defineComponent({
     setup() {
         return {
             iconeLogin,
-            iconeSenha
+            iconeSenha,
+            loginService
         }
     },
     data() {
@@ -20,18 +24,36 @@ export default defineComponent({
         };
     },
     methods: {
-        efetuarLogin() {
-            if (!this.login && !this.senha) {
-                this.erro = "Favor preencher o formulário";
-                return;
+        async efetuarLogin() {
+            try {
+                if (!this.login && !this.login.trim() 
+                    && !this.senha && !this.senha.trim()) {
+                    this.erro = "Favor preencher o formulário";
+                    return;
+                }
+                this.loading = true;
+                await loginService.login({login: this.login, senha: this.senha});
+
+            } catch (e : any) {
+                console.log(e);
+                if(e?.response?.data?.erro){
+                    this.erro = e?.response?.data?.erro;
+                }else{
+                    this.erro = 'Não foi possível efetuar o login, tente novamente!';
+                }
             }
-            alert("Login efetuado: " + this.login + ", " + this.senha);
+            this.loading = false;
         },
-        setLogin(v : any){
+        setLogin(v: any) {
             this.login = v;
         },
-        setSenha(v : any){
+        setSenha(v: any) {
             this.senha = v;
+        }
+    },
+    computed: {
+        buttonText(){
+            return this.loading ? '... Carregando' : 'Login';
         }
     },
     components: { InputPublico }
@@ -42,15 +64,16 @@ export default defineComponent({
     <div class="container-publico">
         <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo" />
         <form>
-            <p v-if="erro" class="error">{{ erro }}</p>
+            <p v-if="erro" class="error">{{  erro  }}</p>
 
-            <InputPublico :icone="iconeLogin" alt="Insira o login" tipo="text" 
-                    placeholder="Email" :modelValue="login" @setInput="setLogin"/>
+            <InputPublico :icone="iconeLogin" alt="Insira o login" tipo="text" placeholder="Email" :modelValue="login"
+                @setInput="setLogin" />
 
-            <InputPublico :icone="iconeSenha" alt="Insira a senha" tipo="password" 
-                    placeholder="Senha" :modelValue="senha" @setInput="setSenha"/>
+            <InputPublico :icone="iconeSenha" alt="Insira a senha" tipo="password" placeholder="Senha"
+                :modelValue="senha" @setInput="setSenha" />
 
-            <button @click.enter.prevent="efetuarLogin">Login</button>
+            <button @click.enter.prevent="efetuarLogin"
+                :disabled="loading">{{buttonText}}</button>
             <div class="link">
                 <p>Não possui uma conta?</p>
                 <a>Faça seu cadastro agora!</a>
@@ -58,6 +81,7 @@ export default defineComponent({
         </form>
     </div>
 </template>
+
 
 
 <style lang="scss" src="@/assets/styles/paginaPublica.scss" />
