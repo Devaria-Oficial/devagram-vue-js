7915b5786bab30249b50cd4f8f7bcd9e983357ce
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-09-08 01:23:46 -0300

    aula03- parte 06 - input customizado e conclusao

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,54 +1,55 @@
 <script lang="ts">
-    import {defineComponent} from 'vue';
+import { defineComponent } from 'vue';
+import InputPublico from '../components/InputPublico.vue';
+import iconeLogin from '../assets/imagens/login.svg';
+import iconeSenha from '../assets/imagens/senha.svg';
 
-    export default defineComponent({
-        data(){
-            return {
-                login: '',
-                senha: '',
-                loading: false,
-                erro : '',
-                inputLoginFocus: false,
-                inputSenhaFocus: false,
-            }
-        },
-        methods: {
-            efetuarLogin(){
-                if(!this.login && !this.senha){
-                    this.erro = 'Favor preencher o formulário';
-                    return;
-                }
-
-                alert('Login efetuado: ' + this.login + ", " + this.senha);
-            },
-            setFocusLogin(v :boolean){
-                this.inputLoginFocus = v;
-            },
-            setFocusSenha(v :boolean){
-                this.inputSenhaFocus = v;
-            }
+export default defineComponent({
+    setup() {
+        return {
+            iconeLogin,
+            iconeSenha
         }
-    });
+    },
+    data() {
+        return {
+            login: "",
+            senha: "",
+            loading: false,
+            erro: ""
+        };
+    },
+    methods: {
+        efetuarLogin() {
+            if (!this.login && !this.senha) {
+                this.erro = "Favor preencher o formulário";
+                return;
+            }
+            alert("Login efetuado: " + this.login + ", " + this.senha);
+        },
+        setLogin(v : any){
+            this.login = v;
+        },
+        setSenha(v : any){
+            this.senha = v;
+        }
+    },
+    components: { InputPublico }
+});
 </script>
 
 <template>
     <div class="container-publico">
-        <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo"/>
+        <img src="../assets/imagens/logo.svg" alt="Logo Devagram" class="logo" />
         <form>
-            <p v-if="erro" class="error">{{erro}}</p>
-            <div class="input" :class="{focus : inputLoginFocus}">
-                <img src="../assets/imagens/login.svg" alt="Insira o login"/>
-                <input type="text" v-model="login" placeholder="Email"
-                    @focus="setFocusLogin(true)"
-                    @blur="setFocusLogin(false)"
-                />
-            </div>
-            <div class="input" :class="{focus : inputSenhaFocus}">
-                <img src="../assets/imagens/senha.svg" alt="Insira a senha"/>
-                <input type="password" v-model="senha" placeholder="Senha"
-                    @focus="setFocusSenha(true)"
-                    @blur="setFocusSenha(false)"/>
-            </div>
+            <p v-if="erro" class="error">{{ erro }}</p>
+
+            <InputPublico :icone="iconeLogin" alt="Insira o login" tipo="text" 
+                    placeholder="Email" :modelValue="login" @setInput="setLogin"/>
+
+            <InputPublico :icone="iconeSenha" alt="Insira a senha" tipo="password" 
+                    placeholder="Senha" :modelValue="senha" @setInput="setSenha"/>
+
             <button @click.enter.prevent="efetuarLogin">Login</button>
             <div class="link">
                 <p>Não possui uma conta?</p>
@@ -57,5 +58,6 @@
         </form>
     </div>
 </template>
+
 
 <style lang="scss" src="@/assets/styles/paginaPublica.scss" />
