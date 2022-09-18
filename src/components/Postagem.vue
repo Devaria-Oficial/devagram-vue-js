d5dc0facb6a37ff25facc5efaf9d8033eb7d4ec0
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-09-18 12:01:12 -0300

    conclusao das rotas de perfil e usuario

--- src/components/Postagem.vue
+++ src/components/Postagem.vue
@@ -7,6 +7,7 @@
     import imgComentario from '../assets/imagens/comentario.svg';
     import imgComentarioAtivo from '../assets/imagens/comentario-ativo.svg';
     import { FeedServices } from '@/services/FeedServices';
+import router from '@/router';
 
     const feedServices = new FeedServices();
     const MAX_DESCRICAO = 90;
@@ -32,6 +33,7 @@
     },
     methods: {
         navegarParaPerfil() {
+            router.push("/usuario/" + this.post?.idUsuario);
         },
         async tooglCurtir(){
             try{
