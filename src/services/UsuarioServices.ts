a94f44585ed305524cfca98e0066935fe43ee8dc
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-09-22 19:55:32 -0300

    aula 07 parte 01

--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -4,4 +4,12 @@ export class UsuarioServices extends HttpApiServices{
     async pesquisar(filtro : String){
         return this.get('/pesquisa?filtro='+ filtro);
     }
+
+    async buscarDadosPorId(id: String){
+        if(id){
+            return await this.get('/pesquisa?id=' + id);
+        }
+
+        return await this.get('/usuario');
+    }
 }
