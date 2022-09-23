6c620e85bd38627450998bf852bf48e9b2e60c43
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-09-22 22:14:15 -0300

    aul 08 parte 02

--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -16,4 +16,8 @@ export class UsuarioServices extends HttpApiServices{
     async togglFollow(id : String){
         return await this.put('/seguir?id='+id);
     }
+
+    async atualizar(body : any){
+        await this.put('/usuario', body);
+    }
 }
