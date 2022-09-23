a824e2f05883d244bad49b59030b4eb633afe2c1
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-09-22 21:53:34 -0300

    aula 08 parte 01

--- src/views/Editar.vue
+++ src/views/Editar.vue
@@ -4,19 +4,40 @@ import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 import { UsuarioServices } from '@/services/UsuarioServices';
 import HeaderAcoes from '../components/HeaderAcoes.vue';
+import Avatar from '../components/Avatar.vue';
 
 const usuarioServices = new UsuarioServices();
 
 export default defineComponent({
-    components: { Header, Footer, HeaderAcoes },
+    components: { Header, Footer, HeaderAcoes, Avatar },
     data() {
         return {
+            nome: localStorage.getItem('nome') as string,
+            avatar: localStorage.getItem('avatar') as string,
+            imagem: {} as any
+        }
+    },
+    computed: {
+        getImagem(){
+            return this.imagem?.preview ? this.imagem?.preview : this.avatar;
         }
     }
 });
 </script>
 <template>
     <Header :hide="true" />
-    <HeaderAcoes :showLeft="false" :showRight="true" :isRightIcon="true" title="Editar Perfil"/>
+    <div class="container-editar">
+        <HeaderAcoes :showLeft="true" :showRight="true" rightLabel="Concluir" title="Editar Perfil"/>
+        <Avatar :imagem="getImagem"/>
+        <button>Alterar Foto de Perfil</button>
+        <input type="file" class="oculto" accept="image/*" ref="referenciaInput"/>
+        <div class="input">
+            <span>Nome</span>
+            <input type="text" v-model="nome" placeholder="Informe seu nome"/>
+            <img alt="limpar" src="../assets/imagens/limpar.svg"/>
+        </div>
+    </div>
     <Footer />
 </template>
+
+<style lang="scss" src="@/assets/styles/editar.scss" />
